68b143c4efbe55182886da5b0c3bf0494e828b12
Nathan <nathan@mattick.name>
2016-12-06 07:39:04 +0000

    Adding equals/hashCode to edge and vertex

--- src/roadgraph/Edge.java
+++ src/roadgraph/Edge.java
@@ -18,20 +18,65 @@ class Edge {
 		this.roadType = roadType;
 		this.length = length;
 	}
-	
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
 	@Override
-	public boolean equals(Object o) {
-		boolean isEqual = false;
-		if ((null != o) && (o instanceof Edge)) {
-			isEqual = 
-					(this.end1 == ((Edge)o).end1)
-					&& (this.end2 == ((Edge)o).end2)
-					&& (this.directed == ((Edge)o).directed)
-					&& (this.roadName == ((Edge)o).roadName)
-					&& (this.roadType == ((Edge)o).roadType)
-					&& (this.length == ((Edge)o).length);
-		}
-		return isEqual;		
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((directed == null) ? 0 : directed.hashCode());
+		result = prime * result + ((end1 == null) ? 0 : end1.hashCode());
+		result = prime * result + ((end2 == null) ? 0 : end2.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(length);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((roadName == null) ? 0 : roadName.hashCode());
+		result = prime * result + ((roadType == null) ? 0 : roadType.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Edge other = (Edge) obj;
+		if (directed == null) {
+			if (other.directed != null)
+				return false;
+		} else if (!directed.equals(other.directed))
+			return false;
+		if (end1 == null) {
+			if (other.end1 != null)
+				return false;
+		} else if (!end1.equals(other.end1))
+			return false;
+		if (end2 == null) {
+			if (other.end2 != null)
+				return false;
+		} else if (!end2.equals(other.end2))
+			return false;
+		if (Double.doubleToLongBits(length) != Double.doubleToLongBits(other.length))
+			return false;
+		if (roadName == null) {
+			if (other.roadName != null)
+				return false;
+		} else if (!roadName.equals(other.roadName))
+			return false;
+		if (roadType == null) {
+			if (other.roadType != null)
+				return false;
+		} else if (!roadType.equals(other.roadType))
+			return false;
+		return true;
 	}
 }
 
--- src/roadgraph/Vertex.java
+++ src/roadgraph/Vertex.java
@@ -3,12 +3,22 @@ package roadgraph;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import geography.GeographicPoint;
 
 class Vertex extends GeographicPoint {
 	private static final long serialVersionUID = 1L;
-	private Integer index;
+	private static int index;
 	private String name;
+	
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	/**
 	 * @return the name
 	 */
@@ -21,12 +31,29 @@ class Vertex extends GeographicPoint {
 	public Vertex(final GeographicPoint inLocation) {
 		super(inLocation.x, inLocation.y);
 		edgeList = new ArrayList<Vertex>();
+		index++;
 	}
 	
 	public void addEdge(final Vertex toVertex) {
 		if (!edgeList.contains(toVertex)) {
 			edgeList.add(toVertex);
 		}
+	}
+	
+	public GeographicPoint getLocation() {
+		return(new GeographicPoint(getX(), getY()));
+	}
+
+	@Override
+	public int hashCode() {
+		return(getLocation().hashCode());
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		// equality on location
+		return((getLocation().getX() == ((Vertex)o).getX()) 
+				&& (getLocation().getY() == ((Vertex)o).getY()));
 	}
 }
 
